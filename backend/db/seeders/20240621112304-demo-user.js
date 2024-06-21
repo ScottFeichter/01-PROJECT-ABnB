62adc57bda6954f9e207998a8b249881c0b8fdfa
Scott Feichter <scottfeichter@gmail.com>
2024-06-21 14:43:45 -0700

    misc

--- backend/db/seeders/20240621112304-demo-user.js
+++ backend/db/seeders/20240621112304-demo-user.js
@@ -23,25 +23,25 @@ module.exports = {
     await User.bulkCreate([
       {
         firstName: 'Demo',
-        lastName: "Lition",
+        lastName: 'Lition',
         email: 'demo@user.io',
         username: 'Demo-lition',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         firstName: 'Fake',
-        lastName: "User1",
+        lastName: 'User1',
         email: 'user1@user.io',
         username: 'FakeUser1',
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         firstName: 'Fake',
-        lastName: "User2",
+        lastName: 'User2',
         email: 'user2@user.io',
         username: 'FakeUser2',
         hashedPassword: bcrypt.hashSync('password3')
-      }
+      },
     ], { validate: true });
   },
 
