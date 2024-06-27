da0639a97942ef65edd5afdaedb4589bcef659c6
Scott Feichter <scottfeichter@gmail.com>
2024-06-27 15:38:50 -0700

    added seeder demo-review. query working need to manipulate the data before res

---
+++ backend/db/seeders/20240627222759-demo-review.js
@@ -0,0 +1,65 @@
+"use strict";
+
+const { Review } = require("../models");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+    await Review.bulkCreate(
+      [
+        {
+          spotId: 1,
+          userId: 1,
+          review: "pretty great",
+          stars: 4,
+        },
+        {
+          spotId: 1,
+          userId: 2,
+          review: "not so great",
+          stars: 1,
+        },
+
+        {
+          spotId: 1,
+          userId: 3,
+          review: "just ok",
+          stars: 3,
+        },
+      ],
+      { validate: true }
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = "Reviews";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        userId: { [Op.in]: [1, 2, 3] },
+      },
+      {}
+    );
+  },
+};
