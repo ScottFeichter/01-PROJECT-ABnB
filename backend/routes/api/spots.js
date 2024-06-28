97783be106ae24c5e1b43bde15e65ae022e25f05
Scott Feichter <scottfeichter@gmail.com>
2024-06-27 17:08:40 -0700

    GET All Spots is working in local

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,15 +3,105 @@ const { Spot, SpotImage, Review, User } = require("../../db/models");
 
 const router = express.Router();
 
-router.get("/", async (req, res) => {
-  const spots = await Spot.findAll({
-    attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'description', 'price', 'createdAt', 'updatedAt', ],
-    include: [{model: SpotImage, attributes: ['url']}, {model: Review, attributes: ['stars']}]
-  });
-  console.log("spots:  ", spots);
+// ==================GET ALL SPOTS=========================================
 
-  res.json(spots);
+// ------------------helpers----------------------------------------------
+
+/**
+ * returns image url or N/A of a spot
+ */
+const previewImage = (spot) => {
+  let previewImage;
+
+  if (spot.SpotImages[0]) {
+    previewImage = spot.SpotImages[0].url;
+  } else previewImage = "N/A";
+
+  return previewImage;
+};
+
+/**
+ * returns average rating or N/A
+ */
+
+const avgRating = (spot) => {
+  let avgRating;
+  if (spot.Reviews.length > 0) {
+    let length = spot.Reviews.length;
+    let sum = 0;
+    let avg;
+    for (let Review of spot.Reviews) {
+      sum = sum + Review.stars;
+    }
+    avg = (sum / length).toFixed(1);
+    avgRating = avg;
+  } else {
+    avgRating = "N/A";
+  }
+  return avgRating;
+};
+
+/**
+ * returns nuSpot
+ */
+
+const nuSpot = (spot) => {
+  let nuSpot = {
+    id: spot.id,
+    ownerId: spot.ownerId,
+    address: spot.address,
+    city: spot.city,
+    state: spot.state,
+    country: spot.country,
+    lat: spot.lat.toFixed(4),
+    lng: spot.lng.toFixed(4),
+    name: spot.name,
+    description: spot.description,
+    price: spot.price,
+    createdAt: spot.createdAt,
+    updatedAt: spot.updatedAt,
+    previewImage: spot.previewImage,
+    avgRating: spot.avgRating,
+  };
+  return nuSpot;
+};
+
+router.get("/", async (req, res) => {
+  let spots = await Spot.findAll({
+    attributes: [
+      "id",
+      "ownerId",
+      "address",
+      "city",
+      "state",
+      "country",
+      "lat",
+      "lng",
+      "name",
+      "description",
+      "price",
+      "createdAt",
+      "updatedAt",
+    ],
+    include: [
+      { model: SpotImage, attributes: ["url"] },
+      { model: Review, attributes: ["stars"] },
+    ],
+  });
+
+  let nuSpots = [];
+
+  for (let spot of spots) {
+    spot.previewImage = previewImage(spot);
+    spot.avgRating = avgRating(spot);
+    nuSpots.push(nuSpot(spot));
+  }
+
+  res.json(nuSpots);
 });
+
+
+
 
 
 
