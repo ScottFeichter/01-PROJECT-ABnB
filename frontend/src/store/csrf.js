f849c7687d4271d84dc8c9c6e2018de6fde5eec3
Scott Feichter <scottfeichter@gmail.com>
2024-07-12 09:04:53 -0700

    phase 0 Wrapping fetch complete

---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,16 @@
+import Cookies from 'js-cookie'
+
+export const csrfFetch = async (url, options = {}) => {
+
+  options.method = options.method || 'GET'
+
+  options.headers = options.headers || {};
+
+  if (options.method.toUpperCase() !== 'GET') {
+    options.headers['Conent-Type'] = options.headers['Content-Type'] || 'applicaiton/json';
+    options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+  }
+  const res = await window.fetch(url, options);
+  if (res.status >= 400) throw res;
+  return res;
+}
