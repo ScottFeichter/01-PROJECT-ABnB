ae970ebaa4d5d5be598ff8421bfb37f9523eb8fa
Scott Feichter <scottfeichter@gmail.com>
2024-07-15 09:10:57 -0700

    logo happening

--- frontend/src/store/components/Navigation/Navigation.jsx
+++ frontend/src/store/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import { NavLink } from "react-router-dom";
 import { useSelector} from "react-redux";
 import ProfileButton from './ProfileButton';
+import logo from '../Logo/scottbnb.png';
 // import OpenModalButton from "../OpenModalButton/OpenModalButton";
 // import LoginFormModal from "../LoginFormModal/LoginFormModal";
 // import SignupFormModal from "../SignupFormModal/SignupFormModal";
@@ -9,16 +10,15 @@ import './Navigation.css';
 
 const Navigation = ({isLoaded}) => {
     const sessionUser = useSelector(state => state.session.user);
-
-
+    // const src = "./Logo/scottbnb.png";
 
     return(
      <header>
-        <img id="logo" src="logo" alt="logo"></img>
+        <img id="logo" src={logo} alt="logo"></img>
         <div id="searchbar">Searchbar</div>
         <ul>
             <li>
-                <NavLink to="/">Home</NavLink>
+                <NavLink to="/" id="NavLinkToHome">Home</NavLink>
             </li>
             {isLoaded && (
             <li>
