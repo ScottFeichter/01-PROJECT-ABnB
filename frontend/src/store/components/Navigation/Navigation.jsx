78be374c7a686c5cc8638f0e6a69b5e83beaa581
Scott Feichter <scottfeichter@gmail.com>
2024-07-14 22:44:24 -0700

    Phase Bonus before OPTIONAL

--- frontend/src/store/components/Navigation/Navigation.jsx
+++ frontend/src/store/components/Navigation/Navigation.jsx
@@ -10,31 +10,6 @@ import './Navigation.css';
 const Navigation = ({isLoaded}) => {
     const sessionUser = useSelector(state => state.session.user);
 
-    let sessionLinks = sessionUser ? (
-        <>
-         <li>
-            <ProfileButton user={sessionUser} />
-         </li>
-        </>
-    ) : (
-        <>
-        <li>
-            <OpenModalButton
-            buttonText="Log In"
-            modalComponent={<LoginFormModal />}
-            />
-        </li>
-        <li>
-            <OpenModalButton
-            buttonText="Sign Up"
-            modalComponent={<SignupFormModal />}
-            />
-        </li>
-        </>
-    );
-
-
-
 
 
     return(
@@ -45,9 +20,12 @@ const Navigation = ({isLoaded}) => {
             <li>
                 <NavLink to="/">Home</NavLink>
             </li>
-            {isLoaded && sessionLinks}
+            {isLoaded && (
+            <li>
+                <ProfileButton user={sessionUser} />
+            </li>
+            )}
         </ul>
-
     </header>
     );
 }
--- frontend/src/store/components/Navigation/ProfileButton.jsx
+++ frontend/src/store/components/Navigation/ProfileButton.jsx
@@ -2,6 +2,9 @@ import { LuUserCircle2 } from "react-icons/lu"
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../session';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 
 import "./ProfileButton.css";
 
@@ -18,8 +21,10 @@ function ProfileButton({ user }) {
   useEffect(() => {
     if (!showMenu) return;
 
+
+    // ulRef.current && this was in the if on line 27
     const closeMenu = (e) => {
-      if (ulRef.current && !ulRef.current.contains(e.target)) {
+      if (!ulRef.current.contains(e.target)) {
       setShowMenu(false);
       }
     };
@@ -43,17 +48,35 @@ function ProfileButton({ user }) {
         <LuUserCircle2 id="LuUserCircle2"/>
       </button>
 
-      <ul className={ulClassName} id="houdini">
-        <li>{user.username}</li>
-        <li>{user.firstName} {user.lastName}</li>
-        <li>{user.email}</li>
-        <li>
-          <button onClick={logout} id="logout">Log Out</button>
-        </li>
+      <ul className={ulClassName} id="houdini" ref={ulRef}>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
       </ul>
     </>
   );
 }
-
 
 export default ProfileButton;
