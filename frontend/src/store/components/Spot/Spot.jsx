fb2b22ff4b03976da2b549f8c25806f07ed0319c
Scott Feichter <scottfeichter@gmail.com>
2024-07-16 13:06:40 -0700

    misc (msg via sh alias)

--- frontend/src/store/components/Spot/Spot.jsx
+++ frontend/src/store/components/Spot/Spot.jsx
@@ -1,4 +1,5 @@
 import './Spot.css';
+import { FaStar } from "react-icons/fa";
 
 
 function Spot({spot}) {
@@ -6,35 +7,34 @@ function Spot({spot}) {
 const spotKeys = Object.keys(spot);
 const spotValues = Object.values(spot);
 // const spotEntries = Object.entries(spot);
+console.log(spot);
 
 
     return (
-        <>
 
-        <div id="spotContainer">
-            <div className="detailContainer"><p className="key">{spotKeys[8]}</p><p className="value">{spotValues[8]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[14]}</p><p className="value">{spotValues[14]}</p></div>
+       <a href={SpotDetails} spot={spot}>
+        <span className="spotContainer">
 
-            <div className="detailContainer"><p className="key">{spotKeys[2]}</p><p className="value">{spotValues[2]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[3]}</p><p className="value">{spotValues[3]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[4]}</p><p className="value">{spotValues[4]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[5]}</p><p className="value">{spotValues[5]}</p></div>
+            <img className="spotPreviewImage" src={spot.previewImage} alt="preview"></img>
 
 
-            <div className="detailContainer"><p className="key">{spotKeys[9]}</p><p className="value">{spotValues[9]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[10]}</p><p className="value">{spotValues[10]}</p></div>
+            <div className='data'>
+                <div className='dataLevelOne'>
+                    <p className='spotCityState'>{spot.city}, {spot.state}</p>
+                    <div className='spotRatingContainer'>
+                        <FaStar className='spotFaStar'></FaStar>
+                        <p className='spotAvgRating'>{spot.avgRating}</p>
+                    </div>
 
-            <div className="detailContainer"><p className="key">{spotKeys[13]}</p><p className="value">{spotValues[13]}</p></div>
+                </div>
 
-            <div className="detailContainer"><p className="key">{spotKeys[0]}</p><p className="value">{spotValues[0]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[1]}</p><p className="value">{spotValues[1]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[6]}</p><p className="value">{spotValues[6]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[7]}</p><p className="value">{spotValues[7]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[11]}</p><p className="value">{spotValues[11]}</p></div>
-            <div className="detailContainer"><p className="key">{spotKeys[12]}</p><p className="value">{spotValues[12]}</p></div>
-        </div>
+                <p className='spotPrice'>${spot.price}/night</p>
+            </div>
 
-        </>
+        </span>
+        </a>
+
+
 
     )
 }
