d5823a32d7cdda44d005a2a010244600a96bc17e
Scott Feichter <scottfeichter@gmail.com>
2024-07-19 03:40:34 -0700

    misc (msg via sh alias)

--- frontend/src/store/getSpotsController.js
+++ frontend/src/store/getSpotsController.js
@@ -1,37 +1,37 @@
 
 
-const getSpotsController = async (search, session, csrf = null) => {
+// const getSpotsController = async (search, session, csrf = null) => {
 
-  if(search) {
-    const {where, checkIn, checkOut, who} = search;
-  }
+//   if(search) {
+//     const {where, checkIn, checkOut, who} = search;
+//   }
 
 
-  // get all spots
-  if(!session) {
-    const response = await fetch("/api/spots");
-    const data = await response.json();
-    console.log('GETSPOTSCONTROLLER RAN - DATA: ', data);
-    return data;
-  }
+//   // get all spots
+//   if(!session) {
+//     const response = await fetch("/api/spots");
+//     const data = await response.json();
+//     console.log('GETSPOTSCONTROLLER RAN - DATA: ', data);
+//     return data;
+//   }
 
-  // get all spots owned by current user /api/spots/current
-  if("true"){
-    const response = await fetch("/api/spots");
-    const data = await response.json();
-    console.log('GETSPOTSCONTROLLER RAN - DATA: ', data);
-    return true
-  }
+//   // get all spots owned by current user /api/spots/current
+//   if("true"){
+//     const response = await fetch("/api/spots");
+//     const data = await response.json();
+//     console.log('GETSPOTSCONTROLLER RAN - DATA: ', data);
+//     return true
+//   }
 
-  // get details of a spot by id /api/spots/:spotId
-  if("true"){
-    const response = await fetch("/api/spots");
-    const data = await response.json();
-    console.log('GETSPOTSCONTROLLER RAN - DATA: ', data);
-    return true
-  }
+//   // get details of a spot by id /api/spots/:spotId
+//   if("true"){
+//     const response = await fetch("/api/spots");
+//     const data = await response.json();
+//     console.log('GETSPOTSCONTROLLER RAN - DATA: ', data);
+//     return true
+//   }
 
-  return null;
-}
+//   return null;
+// }
 
-export default getSpotsController;
+// export default getSpotsController;
